58672c15514a10c0ba34c20b61b5c6d7733c080e
Nicolas Feltman <nfeltman@gmail.com>
2011-12-07 16:56:51 -0500

    Envoy is broken!  dagmanip is good.

---
+++ Code/Shared/dagmanip.py
@@ -0,0 +1,52 @@
+class AddressNode:
+	def __init__(self, p, succ):
+		self.principal = p
+		self.successors = succ
+	
+class AddressDAG:
+	def __init__(self, root, principal_map):
+		self.root = root
+		self.principal_map = principal_map
+	
+def parse_DAG(str):
+	nodes = {}
+	count=-1;
+	for line in str.splitlines():
+		tokens = line.strip().split(" ")
+		princ = tokens.pop(0)
+		succ = []
+		for token in tokens:
+			if(token != '-'):
+				succ.append(int(token))
+		nodes[count] = AddressNode(princ,succ)
+		count = count + 1
+	principal_map = {}
+	for node in nodes.values():
+		node.successors = [nodes[i] for i in node.successors]
+		principal_map[node.principal] = node
+	return AddressDAG(nodes[-1],principal_map)
+
+# internal function, not sure how to do that
+def build_node_list(node, node_list, count):
+	if not (node in node_list):
+		node_list[node] = count;
+		count = count + 1;
+		for sub_node in node.successors:
+			count = build_node_list(sub_node, node_list, count)
+	return count;
+
+def DAG_to_string(dag):
+	node_dict = {}
+	final_count = build_node_list(dag.root, node_dict, -1)
+	line_dict = {}
+	for node, index in node_dict.items():
+		successors = ''.join(['%i ' % node_dict[succ] for succ in node.successors])
+		line_dict[index] = ('%s %s- ' % (node.principal, successors))
+	res = line_dict[-1]
+	for index in range(0,final_count):
+		res = res + '\n' + line_dict[index]
+	return res
+
+# examples
+dag=parse_DAG("DAG 0 1 - \n AD 2 - \n IP 2 - \n HID 3 - \n CID")
+print(DAG_to_string(dag))
